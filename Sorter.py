24f78764f32a015c82f7522db48cf8b41d1d2e35
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-10-31 22:51:09 -0600

    Refactoring selectionSort method
    selectionSort method now uses original intList argument and moves values around instead of the old method of creating a whole new list and deleting values inside of the old list.

--- Sorter.py
+++ Sorter.py
@@ -1,38 +1,31 @@
-from dataclasses import dataclass
-
-@dataclass
 class Sorter:
-    def selectionSort(self, listOfInts: list[int], option: str="ascending") -> list[int]:
-        sortedList = []
+    # Selection sorting method. Time complexity : O(n^2)
+    def selectionSort(self, intList: list[int], option: str="ascending") -> list[int]:
+        """
+        Sorts out a list using selection sorting method.
+        Time Complexity : O(n^2) 
+        """
+        
+        for index in range(len(intList)):
+            minimumIntegerIndex = index
 
-        if option.lower() == "ascending":
-            while len(listOfInts) != 0:
-                compareInteger = listOfInts[0]
+            assert option == "ascending" or option == "descending", "option should be either ascending or descending"
 
-                for integer in listOfInts:
-                    if integer < compareInteger:
-                        compareInteger = integer
-                
-                
-                listOfInts.remove(compareInteger)
-                sortedList.append(compareInteger)
-        if option.lower() == "descending":
-            while len(listOfInts) != 0:
-                compareInteger = listOfInts[0]
-
-                for integer in listOfInts:
-                    if integer > compareInteger:
-                        compareInteger = integer
-                
-                
-                listOfInts.remove(compareInteger)
-                sortedList.append(compareInteger)
-
-        return sortedList
+            for secondIndex in range(len(intList)):
+                if index < secondIndex:
+                    if option.lower() == "ascending":
+                        if intList[secondIndex] < intList[minimumIntegerIndex]:
+                            minimumIntegerIndex = secondIndex
+                    if option.lower() == "descending":
+                        if intList[secondIndex] > intList[minimumIntegerIndex]:
+                            minimumIntegerIndex = secondIndex
+            
+            intList.insert(index, intList.pop(minimumIntegerIndex))
+        return intList
     
 def main() -> None:
     sorter = Sorter()
-    print(sorter.selectionSort([23, 12, 87, 63, 54]))
+    print(sorter.selectionSort([23, 12, 87, 63, 54, 84, 79, 28, 11, 1, 0, -12, 32, -40], "ascending"))
 
 if __name__ == "__main__":
     main()
